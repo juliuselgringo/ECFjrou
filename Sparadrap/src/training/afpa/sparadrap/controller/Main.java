42207096485efce4d5a27b04a1213ecad258e349
juliuselgringo <julien.rousselot@hotmail.fr>
2025-09-01 16:34:23 +0200

    feat: models

--- Sparadrap/src/training/afpa/sparadrap/controller/Main.java
+++ Sparadrap/src/training/afpa/sparadrap/controller/Main.java
@@ -1,8 +1,22 @@
 package training.afpa.sparadrap.controller;
 
+
+import java.time.LocalDate;
+
 public class Main {
 
     public static void main(String[] args) {
+        /*
+        String dateinput = "1980-12-20";
+        LocalDate date = LocalDate.parse(dateinput);
+        LocalDate now = LocalDate.now();
+        if (date.isBefore(now)) {
+            System.out.println("hinnnnn");
+        }else{
+            System.out.println(date);
+        }
+        */
+
 
 
     }
--- Sparadrap/src/training/afpa/sparadrap/model/Customer.java
+++ Sparadrap/src/training/afpa/sparadrap/model/Customer.java
@@ -1,11 +1,81 @@
 package training.afpa.sparadrap.model;
 
+import training.afpa.sparadrap.ExceptionTracking.InputException;
+
+import java.time.LocalDate;
 import java.util.Date;
 
-public class Customer {
+public class Customer extends Person {
 
     private String socialSecurityId;
-    private Date dateOfBirth;
+    private LocalDate dateOfBirth;
     private Mutual mutual;
+    private Doctor doctor;
+
+    private static final String regexSocialSecurityId = "^[12]\\d{2}(0[1-9]|1[0-2])\\d{2}\\d{3}\\d{3}\\d{3}$\n";
+
+    /**
+     * CONSTRUCTOR
+     * @param socialSecurityId String
+     * @param dateOfBirth Date
+     * @param mutual Mutual
+     * @param doctor Doctor
+     * @throws InputException
+     */
+    public Customer(String firstName, String lastName, Contact contact,String socialSecurityId,
+                    LocalDate dateOfBirth, Mutual mutual, Doctor doctor) throws InputException {
+        super(firstName, lastName, contact);
+        setSocialSecurityId(socialSecurityId);
+        setDateOfBirth(dateOfBirth);
+    }
+
+    /**
+     * GETTER socialSecurityId
+     * @return String
+     */
+    public String getSocialSecurityId() {
+        return socialSecurityId;
+    }
+
+    /**
+     * SETTER socialSecurityId
+     * @param socialSecurityId String
+     * @throws InputException
+     */
+    public void setSocialSecurityId(String socialSecurityId) throws InputException {
+        socialSecurityId = socialSecurityId.trim();
+        if (socialSecurityId.isEmpty() || socialSecurityId == null) {
+            throw new InputException("socialSecurityId cannot be empty or null");
+        } else if (!socialSecurityId.matches(regexSocialSecurityId)) {
+            throw new InputException("socialSecurityId is not valid");
+        } else {
+            this.socialSecurityId = socialSecurityId;
+        }
+    }
+
+    /**
+     * GETTER dateOfBirth
+     * @return LocalDate
+     */
+    public LocalDate getDateOfBirth() {
+        return this.dateOfBirth;
+    }
+
+    /**
+     * SETTER dateOfBirth
+     * @param dateOfBirth LocalDate
+     * @throws InputException
+     */
+    public void setDateOfBirth(LocalDate dateOfBirth) throws InputException {
+        if(dateOfBirth == null) {
+            throw new InputException("dateOfBirth cannot be null");
+        } else if (dateOfBirth.isAfter(LocalDate.now())) {
+            throw new InputException("dateOfBirth is after now");
+        }else{
+            this.dateOfBirth = dateOfBirth;
+        }
+    }
+
+
 
 }
